228b76a4651007bec95df4655df8f041819f2208
Phil <Phil.whipps@unifysolutions.net>
2017-02-06 11:52:25 +1000

    - Renamed Function to get Access Token to remain internal to Module - Added Function  Set-B2CDefaultTenant - Added Function Set-B2CPolicy

--- Powershell/B2CUtils.psm1
+++ Powershell/B2CUtils.psm1
@@ -5,7 +5,7 @@ Written by Phil Whipps
 UNIFY Solutions
 
 #>
-function Get-B2CAccessToken
+function Get-TenantAccessToken
 {
 param(
     $TenantId,
@@ -14,11 +14,11 @@ param(
 
 
 	$adal = "$PSScriptRoot\Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
-	if (!(Test-Path $adal)) { "Could not find $adal. Please make sure you run this obtain the full ExploreAdmin folder and run the script from there."; return;}
+	if (!(Test-Path $adal)) { Throw "Could not find $adal. Please make sure you run this obtain the full ExploreAdmin folder and run the script from there."; return;}
 	[System.Reflection.Assembly]::LoadFile($adal) > $null
 
 	$adalWin = "$PSScriptRoot\Microsoft.IdentityModel.Clients.ActiveDirectory.WindowsForms.dll";
-	if (!(Test-Path $adal)) { "Could not find $adal. Please make sure you run this obtain the full ExploreAdmin folder and run the script from there."; return;}
+	if (!(Test-Path $adal)) { Throw "Could not find $adal. Please make sure you run this obtain the full ExploreAdmin folder and run the script from there."; return;}
 	[System.Reflection.Assembly]::LoadFile($adalWin) > $null
 
 
@@ -45,6 +45,40 @@ param(
 
 	$token.AccessToken;
 
+}
+
+
+
+
+<# 
+ .Synopsis
+  Sets a default tenant for B2CUtils.
+
+ .Description
+  Because I am lazy and do not want to enter the TenantId each Time, this allows me to set the default tenant to be used..
+
+ .Parameter TenantId
+  The name of the B2C Tenant.
+  
+
+ .Example
+   # Set the default Tenant.
+   Set-B2CDefaultTenant -TenantId "myb2ctenant.onmicrosoft.com"
+
+ .LINK
+	https://github.com/WhippsP/B2CUtils
+   
+#>
+
+function Set-B2CDefaultTenant {
+param(
+   [Parameter(Mandatory=$true)]
+    [AllowEmptyString()]
+    [string]$TenantId
+    )
+	#Export Default Values for Functions )
+	$global:PSDefaultParameterValues = @{"*-B2C*:TenantId"=$TenantId}
+	
 }
 
 
@@ -81,7 +115,7 @@ param(
 	[switch]$ForeAuthn
     )
 
-$accessToken = Get-B2CAccessToken -TenantId $TenantId -ForeAuthn $ForeAuthn
+$accessToken = Get-TenantAccessToken -TenantId $TenantId -ForeAuthn $ForeAuthn
 
 
 $KeyList = Invoke-WebRequest "https://main.b2cadmin.ext.azure.com/api/keylist?api-version=1&TenantId=$TenantId&KeyType=cert" -Method Get -Headers @{Authorization="Bearer $accessToken"}
@@ -125,7 +159,7 @@ param(
 	[switch]$ForeAuthn
     )
 
-$accessToken = Get-B2CAccessToken -TenantId $TenantId -ForeAuthn $ForeAuthn
+$accessToken = Get-TenantAccessToken -TenantId $TenantId -ForeAuthn $ForeAuthn
 
 $PolicyList = Invoke-WebRequest "https://main.b2cadmin.ext.azure.com/api/policyList?tenantId=$TenantId" -Method Get -Headers @{Authorization="Bearer $accessToken"}
 
@@ -140,7 +174,7 @@ $PolicyList = Invoke-WebRequest "https://main.b2cadmin.ext.azure.com/api/policyL
   Gets a B2C Policy.
 
  .Description
-  REtrives a Trust Framework Policy from B2C.
+  Retrives a Trust Framework Policy from B2C.
 
  .Parameter TenantId
   The name of the B2C Tenant.
@@ -181,10 +215,112 @@ param(
 	[switch]$ForeAuthn
     )
 
-	$accessToken = Get-B2CAccessToken -TenantId $TenantId -ForeAuthn $ForeAuthn
-	
-$pol = Invoke-WebRequest "https://cpim.windows.net/api/TrustFramework/GetAsXml?api-version=1&TenantId=$TenantId&PolicyId=$PolicyId&SendAsAttachment=false&GetBasePolicies=$GetBasePolicies" -Method Get -Headers @{Authorization="Bearer $accessToken"}
+	$accessToken = Get-TenantAccessToken -TenantId $TenantId -ForeAuthn $ForeAuthn
+
+$pol = Invoke-WebRequest "https://main.b2cadmin.ext.azure.com/api/TrustFramework/GetAsXml?api-version=1&TenantId=$TenantId&PolicyId=$PolicyId&SendAsAttachment=false&GetBasePolicies=$GetBasePolicies" -Method Get -Headers @{Authorization="Bearer $accessToken"}
 ([xml]$pol.content)
 }
 
-export-modulemember -Function Get-B2CCertList, Get-B2CPolicyList, Get-B2CPolicy
+
+<# 
+ .Synopsis
+  Upload a policy to B2C.
+
+ .Description
+  This function uploads a B2C Policy.
+
+ .Parameter TenantId
+  The name of the B2C Tenant.
+  
+  .Parameter Policy
+  Specify the Policy XML Content.
+  
+  .Parameter OverwriePolicy
+  Overwrites the Policy - Default is $True.
+
+ .Parameter ForeAuthn
+  Forces you to re-authenticate.
+  
+
+ .Example
+   # Upload a Policies.
+   $policy = Get-Content -raw "C:\Temp\B2CPolicy.xml"
+   Set-B2CPolicy -TenantId "myb2ctenant.onmicrosoft.com" -Policy $policy
+
+ .Example
+   # Upload a Policies with .
+   $policy = Get-Content -raw "C:\Temp\B2CPolicy.xml" 
+   Set-B2CPolicy -TenantId "myb2ctenant.onmicrosoft.com" -Policy $policy -OverwriePolicy $False
+   
+ .LINK
+	https://github.com/WhippsP/B2CUtils
+   
+#>
+function Set-B2CPolicy {
+param(
+	[Parameter(Mandatory=$true)]
+    [string]$TenantId,
+	[Parameter(Mandatory=$true)]
+	[string]$Policy,
+	[bool]$OverwriePolicy=$true,
+	[switch]$ForeAuthn
+    )
+	Add-Type -AssemblyName System.Web
+	$accessToken = Get-TenantAccessToken -TenantId $TenantId -ForeAuthn $ForeAuthn
+	
+	# Policy Must be HTML Encoded within a String Element
+	
+    $PostStr = '<string xmlns="http://schemas.microsoft.com/2003/10/Serialization/">' + [System.Web.HttpUtility]::HtmlEncode($Policy) + '</string>'
+	
+	
+	try{
+		$webAppResponse = Invoke-WebRequest "https://main.b2cadmin.ext.azure.com/api/trustframework?tenantId=$TenantId&OverwriteIfExists=$OverwriePolicy" -Method Post -Headers @{Authorization="Bearer $accessToken"} -ContentType "application/xml" -Body $PostStr
+	}
+	catch {
+			#Report Returned Error to The User
+			$result = $_.Exception.Response.GetResponseStream()
+			$reader = New-Object System.IO.StreamReader($result)
+			$reader.BaseStream.Position = 0
+			$reader.DiscardBufferedData()
+			$responseBody = $reader.ReadToEnd();
+			Write-Error ([xml]$responseBody).Error.ExceptionMessage
+	}
+
+}
+
+function Remove-B2CPolicy {
+
+}
+
+function Remove-B2CCertificate {
+
+}
+
+function New-B2CCertificate {
+
+}
+
+function Get-B2CCertificate {
+
+}
+
+function Remove-B2CKeyContainer {
+
+}
+
+function New-B2CKeyContainer {
+
+}
+
+function Set-B2CKeyContainer {
+
+}
+
+function Get-B2CKeyContainer {
+
+}
+
+
+
+export-modulemember -Function *-B2C*
+
